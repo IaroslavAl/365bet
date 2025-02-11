e126947bee377076b1c40d1ae45ee17170d56d41
Iaroslav Beldin <Iaroslav.aleksandrovich.beldin@gmail.com>
2025-02-11 22:38:23 +0300

    Add Hamburger Menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,32 @@
 import "./Header.css"
 import logo from "/logo.svg"
 import Button from "../Button/Button.jsx"
+import {useState} from "react"
 import {Link, useLocation} from "react-router-dom"
 
 export default function Header() {
     const location = useLocation()
     const activeTab = location.pathname.split("/")[1]
+    const [menuOpen, setMenuOpen] = useState(false)
 
     return (
         <header className="header">
             <div className="logo">
                 <img src={logo} alt="Logo"/>
             </div>
-            <nav className="nav">
+            <button className="burger-menu" onClick={() => setMenuOpen(!menuOpen)}>
+                ☰
+            </button>
+            <nav className={`nav ${menuOpen ? "open" : ""}`}>
                 <ul>
-                    <li><Link to=""><Button isActive={!["rpl", "la-liga", "apl"].includes(activeTab)}>Футбол</Button></Link></li>
-                    <li><Link to="/rpl"><Button isActive={activeTab === "rpl"}>РПЛ</Button></Link></li>
-                    <li><Link to="/la-liga"><Button isActive={activeTab === "la-liga"}>Ла Лига</Button></Link></li>
-                    <li><Link to="/apl"><Button isActive={activeTab === "apl"}>АПЛ</Button></Link></li>
+                    <li><Link to="" onClick={() => setMenuOpen(false)}><Button
+                        isActive={!["rpl", "la-liga", "apl"].includes(activeTab)}>Футбол</Button></Link></li>
+                    <li><Link to="/rpl" onClick={() => setMenuOpen(false)}><Button
+                        isActive={activeTab === "rpl"}>РПЛ</Button></Link></li>
+                    <li><Link to="/la-liga" onClick={() => setMenuOpen(false)}><Button
+                        isActive={activeTab === "la-liga"}>Ла Лига</Button></Link></li>
+                    <li><Link to="/apl" onClick={() => setMenuOpen(false)}><Button
+                        isActive={activeTab === "apl"}>АПЛ</Button></Link></li>
                 </ul>
             </nav>
         </header>
